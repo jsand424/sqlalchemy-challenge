5fa1e473a570c6da081eba61a748c36efc4e20e3
jsand424 <sandlerjackson@gmail.com>
2024-07-30 15:22:04 +1000

    Add files via upload

---
+++ Surfsup/app.py
@@ -0,0 +1,119 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import datetime as dt
+
+
+#################################################
+# Database Setup
+#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Declare a Base using `automap_base()`
+Base = automap_base()
+
+# Use the Base class to reflect the database tables
+Base.prepare(engine, reflect=True)
+
+# Assign the measurement class to a variable called `Measurement` and
+# the station class to a variable called `Station`
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Create a session
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= one_year_ago).all()
+
+    session.close()
+
+    precipitation_dict = {date: prcp for date, prcp in precipitation_data}
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
+
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+
+    most_active_station_id = session.query(Measurement.station, func.count(Measurement.station))\
+        .group_by(Measurement.station)\
+        .order_by(func.count(Measurement.station).desc()).first()[0]
+
+    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.station == most_active_station_id,
+        Measurement.date >= one_year_ago).all()
+    
+    session.close()
+
+    tobs_list = list(np.ravel(tobs_data))
+    return jsonify(tobs_list)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_range(start, end=None):
+    session = Session(engine)
+    if end:
+        temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+            .filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    else:
+        temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+            .filter(Measurement.date >= start).all()
+    
+    session.close()
+
+    temp_list = list(np.ravel(temps))
+    return jsonify(temp_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
